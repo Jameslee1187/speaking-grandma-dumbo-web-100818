07038cc8dca5321f4a93aad9338512060d0817d4
jameslee1187@gmail.com <jameslee1187@gmail.com>
2018-09-05 02:47:35 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -4,10 +4,10 @@ def grandma(phrase)
   elsif phrase == phrase.upcase
     return "NO, NOT SINCE 1938!"
   else
-    phrase == "I LOVE YOU GRANDMA!"
+    if phrase == "I LOVE YOU GRANDMA!"
       return "I LOVE YOU TOO PUMPKIN!"
     end
-  
+  end
 end
       
 # Write a speak_to_grandma method.
